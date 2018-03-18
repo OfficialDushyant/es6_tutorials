2e4a5fc99c53cc6523d216fad760ac3a2f0f4e41
Dushynat <msg2dushyant@gmail.com>
2018-03-18 15:52:32 -0400

    Setup for gulp babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-let gulp = require('gulp');
-let sass = require('gulp-sass');
-let uglifycss = require('gulp-uglifycss');
-let browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const uglifycss = require('gulp-uglifycss');
+const browserSync = require('browser-sync').create();
+const babel = require('gulp-babel');
 
 //Compile scss to css
 gulp.task('sass', () => {
@@ -30,11 +33,27 @@ gulp.task('browserSync', () => {
       baseDir: 'app'
     },
   });
-  gulp.watch("app/*.html").on('change', browserSync.reload);
+  gulp.watch("app/**/*.html").on('change', browserSync.reload);
+  gulp.watch("app/**/*.js").on('change', browserSync.reload);
+  gulp.watch("app/**/*.scss").on('change', browserSync.reload);
 });
 
+//Babel transpiler
+gulp.task('composeJS', () =>
+    gulp.src('app/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            plugins: ['@babel/transform-runtime'],
+            presets: ['@babel/env']
+        }))
+        .pipe(concat('all.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'))
+);
+
 //Gulp watch
-gulp.task('watch',['browserSync','sass'] ,() => {
+gulp.task('watch',['browserSync'] ,() => {
+  guulp.watch('app/**/*.js', ['composeJS'])
   gulp.watch('app/styles/**/*.scss', ['sass']);
   gulp.watch('app/styles/css/*.css', ['mincss']);
 });
